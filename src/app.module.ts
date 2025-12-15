12483c211f9f58e4b3df1a84f81b10362df69c4a
Zain Fareed <zainfareed093@gmail.com>
2025-12-16 04:47:52 +0500

    ci/cd issues fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -15,20 +15,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST', 'localhost'),
-        port: +configService.get<number>('DB_PORT', 5432),
-        username: configService.get('DB_USERNAME', 'postgres'),
-        password: configService.get('DB_PASSWORD', ''),
-        database: configService.get('DB_DATABASE', 'nexus'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: false, // Disabled because Prisma manages the schema
-      }),
-      inject: [ConfigService],
-    }),
+
     AuthModule,
     ProjectsModule,
     TasksModule,
